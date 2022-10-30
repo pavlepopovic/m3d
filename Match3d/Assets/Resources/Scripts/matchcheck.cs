08287f9319076fbcb0df97149d1498364a80d230
unknown <pavlepopovic9777@gmail.com>
2022-10-30 17:03:27 +0100

    MatchCheck

--- Match3d/Assets/Resources/Scripts/matchcheck.cs
+++ Match3d/Assets/Resources/Scripts/matchcheck.cs
@@ -3,99 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class matchcheck : MonoBehaviour
-{
-    int collectedobjectvalue=0;
-    
-    [Header("Object that have Placed")]
-    public List<GameObject> PlaceObject = new List<GameObject>();
+public class MatchCheck : MonoBehaviour
+{    
     [Header("Destination point")]
     public GameObject PointA;
     public GameObject PointB;
-    public GameObject Matchpoint;
+
+    [UnityEngine.Serialization.FormerlySerializedAs("Matchpoint")]
+    public GameObject MatchPoint;
 
     [Header("Match Anim")]
-    public GameObject matchanim;
-    public GameObject Staranim;
-    public Text Starvalue;
-    int starvalue=0;
+    [UnityEngine.Serialization.FormerlySerializedAs("matchanim")]
+    public GameObject MatchAnim;
+    [UnityEngine.Serialization.FormerlySerializedAs("Staranim")]
+    public GameObject StarAnim;
+    [UnityEngine.Serialization.FormerlySerializedAs("Starvalue")]
+    public Text StarValue;
+
     [Header("Hint Stuff")]
-    public Button hintbutton;
-    public GameObject Addrewardhintdialogbox;
-    public GameObject Spawnpoint;
-    public int automovingobjectspeed;
-    public List<GameObject> HintObjects = new List<GameObject>();
+    [UnityEngine.Serialization.FormerlySerializedAs("hintbutton")]
+    public Button HintButton;
+    [UnityEngine.Serialization.FormerlySerializedAs("Addrewardhintdialogbox")]
+    public GameObject AddRewardHintDialogBox;
+    [UnityEngine.Serialization.FormerlySerializedAs("Spawnpoint")]
+    public GameObject SpawnPoint;
 
-
-    bool firstplace, secondplace;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-    }
+    private List<GameObject> m_HintObjects = new List<GameObject>();
+    private List<GameObject> m_PlaceObject = new List<GameObject>();
+    private int m_CollectedObjectValue = 0;
+    private int m_StarValue = 0;
 
     private void OnTriggerStay(Collider other)
     {
-        if (PlaceObject.Count == 0)
+        if(m_PlaceObject.Count == 0 || (other.gameObject.name.Contains(m_PlaceObject[0].name) == true))
         {
-
+            return;
         }
-        else if (other.gameObject.name.Contains(PlaceObject[0].name) == true)
-        {
-        }
-        else
-        {
-            other.GetComponent<Rigidbody>().velocity = new Vector3(0, 1, 1) * 120 * Time.deltaTime;
-        }
+        other.GetComponent<Rigidbody>().velocity = new Vector3(0, 1, 1) * 120 * Time.deltaTime;
     }
 
-            private void OnTriggerEnter(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (PlaceObject.Count == 0)
-        {
-           
-                other.gameObject.transform.position = PointA.transform.position;
-                other.gameObject.transform.rotation = PointA.transform.rotation;
+        if(m_PlaceObject.Count == 0)
+        {           
+            other.gameObject.transform.position = PointA.transform.position;
+            other.gameObject.transform.rotation = PointA.transform.rotation;
             other.gameObject.GetComponent<Item>().SetRotation(); 
-            other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-           
-                PlaceObject.Add(other.gameObject);
-          
-        }else if (other.gameObject.name.Contains(PlaceObject[0].name)==true)
+            other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;           
+            m_PlaceObject.Add(other.gameObject);          
+        }
+        else if(other.gameObject.name.Contains(m_PlaceObject[0].name) == true)
         {
-            
-               
-                other.gameObject.transform.position = PointB.transform.position;
-                other.gameObject.transform.rotation = PointB.transform.rotation;
+            other.gameObject.transform.position = PointB.transform.position;
+            other.gameObject.transform.rotation = PointB.transform.rotation;
             other.gameObject.GetComponent<Item>().SetRotation();
             other.gameObject.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
-
             other.gameObject.GetComponent<MeshCollider>().enabled = false;
+            m_PlaceObject.Add(other.gameObject);
+            m_PlaceObject[0].GetComponent<MeshCollider>().enabled = false;
+            m_PlaceObject[1].GetComponent<MeshCollider>().enabled = false;
 
-           
-
-            PlaceObject.Add(other.gameObject);
-
-            PlaceObject[0].GetComponent<MeshCollider>().enabled = false;
-            PlaceObject[1].GetComponent<MeshCollider>().enabled = false;
-
-            Invoke("collectObject", 0.2f);
-                Invoke("disableanim", 0.4f);
-
-
-                StartCoroutine(movetomatchpoint(0.01f, PlaceObject[0], PlaceObject[1]));
+            Invoke("CollectObject", 0.2f);
+            Invoke("DisableAnim", 0.4f);
+            StartCoroutine(MoveToMatchPoint(0.01f, m_PlaceObject[0], m_PlaceObject[1]));
 
             SoundManager.instance.PlaybottlfillSOund();
-                Invoke("disablestaranim", 1f);
-
-                matchanim.GetComponent<Animator>().SetBool("collect", true);
-                Staranim.SetActive(true);
-            
+            Invoke("DisableStarAnim", 1f);
+            MatchAnim.GetComponent<Animator>().SetBool("collect", true);
+            StarAnim.SetActive(true);            
         }
         else
         {
@@ -103,7 +78,7 @@ public class matchcheck : MonoBehaviour
             other.gameObject.transform.rotation = PointB.transform.rotation;
 
             SoundManager.instance.wrongmatchsound();
-            if(prefmanager.instance.Getvibrationsvalue()==1)
+            if(prefmanager.instance.Getvibrationsvalue() == 1)
             {
                 Handheld.Vibrate();
             }
@@ -111,62 +86,49 @@ public class matchcheck : MonoBehaviour
         }
     }
 
-
     private void OnTriggerExit(Collider other)
     {
-        if (PlaceObject.Contains(other.gameObject))
-        {
-             
-            PlaceObject.Remove(other.gameObject);
-             
+        if (m_PlaceObject.Contains(other.gameObject))
+        {             
+            m_PlaceObject.Remove(other.gameObject);             
         }
     }
 
-
-    //Collect And Destroy Objects
-    void collectObject()
+    void CollectObject()
     {
-
-        Destroy(PlaceObject[0].gameObject);
-       
-            Destroy(PlaceObject[1].gameObject);
-       
-       
-            PlaceObject.Clear();
-            HintObjects.Clear();
-      
+        Destroy(m_PlaceObject[0].gameObject);       
+        Destroy(m_PlaceObject[1].gameObject);
+        m_PlaceObject.Clear();
+        m_HintObjects.Clear();
     }
    
-    void disableanim()
+    void DisableAnim()
     {
-        matchanim.GetComponent<Animator>().SetBool("collect", false);
+        MatchAnim.GetComponent<Animator>().SetBool("collect", false);
     }
 
-    void disablestaranim()
+    void DisableStarAnim()
     {
-        Staranim.SetActive(false);
-        starvalue++;
-        Starvalue.text = starvalue.ToString();
-        collectedobjectvalue++;
-        GameManager.s_Instance.CheckLevelComplete(collectedobjectvalue);
+        StarAnim.SetActive(false);
+        m_StarValue++;
+        StarValue.text = m_StarValue.ToString();
+        m_CollectedObjectValue++;
+        GameManager.s_Instance.CheckLevelComplete(m_CollectedObjectValue);
     }
 
-    public void OnhintClick()
+    public void OnHintClick()
     {
-        if (prefmanager.instance.Gethintvalue() > 0)
-           
-            {
-
-            hintbutton.interactable = false;
+        if (prefmanager.instance.Gethintvalue() > 0)           
+        {
+            HintButton.interactable = false;
             int hintvalue = prefmanager.instance.Gethintvalue();
             hintvalue--;
             prefmanager.instance.SetHintValue(hintvalue);
 
-
             FindGameObjects();
-            if (PlaceObject.Count != 1)
+            if (m_PlaceObject.Count != 1)
             {
-                translateobjecttopoint(HintObjects[0], PointA.transform.GetChild(0).gameObject);
+                TranslateObjectToPoint(m_HintObjects[0], PointA.transform.GetChild(0).gameObject);
                 Invoke("PlaceOtherObject", 1f);
             }
             else
@@ -176,50 +138,50 @@ public class matchcheck : MonoBehaviour
         }
         else
         {
-            Addrewardhintdialogbox.SetActive(true);
+            AddRewardHintDialogBox.SetActive(true);
         }
     }
 
     void PlaceOtherObject()
     {
-        translateobjecttopoint(HintObjects[1],PointB.transform.GetChild(0).gameObject);
+        TranslateObjectToPoint(m_HintObjects[1],PointB.transform.GetChild(0).gameObject);
         Invoke("MakeHintButtonInteractable", 2f);
     }
 
     void MakeHintButtonInteractable()
     {
-        hintbutton.interactable = true;
+        HintButton.interactable = true;
     }
 
     void FindGameObjects()
     {
         int i = 0;
-        HintObjects.Clear();
-        if (PlaceObject.Count == 1)
+        m_HintObjects.Clear();
+        if (m_PlaceObject.Count == 1)
         {
-            HintObjects.Add(PlaceObject[0].gameObject);
+            m_HintObjects.Add(m_PlaceObject[0].gameObject);
         }
         else
         {
             i++;
-            HintObjects.Add(Spawnpoint.transform.GetChild(0).gameObject);
+            m_HintObjects.Add(SpawnPoint.transform.GetChild(0).gameObject);
         }
         
-        while( i < Spawnpoint.transform.childCount)
+        while(i < SpawnPoint.transform.childCount)
         {
-            if(Spawnpoint.transform.GetChild(i).name== HintObjects[0].name )
+            if(SpawnPoint.transform.GetChild(i).name== m_HintObjects[0].name )
             {
-                if (PlaceObject.Count==1)
+                if (m_PlaceObject.Count==1)
                 {
-                    if(!GameObject.ReferenceEquals(Spawnpoint.transform.GetChild(i).gameObject,PlaceObject[0].gameObject))
+                    if(!ReferenceEquals(SpawnPoint.transform.GetChild(i).gameObject,m_PlaceObject[0].gameObject))
                     {
-                        HintObjects.Add(Spawnpoint.transform.GetChild(i).gameObject);
+                        m_HintObjects.Add(SpawnPoint.transform.GetChild(i).gameObject);
                         break;
                     }
                 }
                 else
                 {
-                    HintObjects.Add(Spawnpoint.transform.GetChild(i).gameObject);
+                    m_HintObjects.Add(SpawnPoint.transform.GetChild(i).gameObject);
                     break;
                 }
             }
@@ -227,20 +189,21 @@ public class matchcheck : MonoBehaviour
         }
     }
 
-    //MOving Object to Center of point to give Merging illusion
-    IEnumerator movetomatchpoint(float delayTime, GameObject a, GameObject b)
+    // Moving object to center of point to give merging illusion
+    IEnumerator MoveToMatchPoint(float delayTime, GameObject a, GameObject b)
     {
         yield return new WaitForSeconds(delayTime); // start at time X
         float startTime = Time.time; // Time.time contains current frame time, so remember starting point
         while (Time.time - startTime <= 0.17)
-        { // until one second passed
-            a.transform.position = Vector3.Lerp(a.transform.position, Matchpoint.transform.position, Time.time - startTime); // lerp from A to B in one second
-            b.transform.position = Vector3.Lerp(b.transform.position, Matchpoint.transform.position, Time.time - startTime); // lerp from A to B in one second
+        { 
+            // until one second passed
+            a.transform.position = Vector3.Lerp(a.transform.position, MatchPoint.transform.position, Time.time - startTime); // lerp from A to B in one second
+            b.transform.position = Vector3.Lerp(b.transform.position, MatchPoint.transform.position, Time.time - startTime); // lerp from A to B in one second
             yield return 0.17f; // wait for next frame           
         }
     }
 
-    void translateobjecttopoint(GameObject g,GameObject point)
+    void TranslateObjectToPoint(GameObject g, GameObject point)
     {
         g.gameObject.GetComponent<Item>().HintBool = true;
         g.gameObject.GetComponent<Item>().OnMouseDown();
@@ -248,7 +211,7 @@ public class matchcheck : MonoBehaviour
         StartCoroutine(WaitAndMove(0.05f,g, point));
     }
 
-    IEnumerator WaitAndMove(float delayTime,GameObject g,GameObject point)
+    IEnumerator WaitAndMove(float delayTime, GameObject g, GameObject point)
     {
         yield return new WaitForSeconds(delayTime); // start at time X
         float startTime = Time.time; // Time.time contains current frame time, so remember starting point
